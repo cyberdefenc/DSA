f04c9b7a025166fac4ef211871347b1c51422d7d
UtkarshcyberArena <utkarshkushwahaofficial29@gmail.com>
2025-05-26 06:41:04 +0530

    newone

---
+++ DSA with Java/Day0002/P3_kth_max_min_.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class InputArray {
+
+    
+    InputArray(int[] arr, Scanner sc) {
+        System.out.println("Enter " + arr.length + " elements:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+    }
+
+    
+
+}
+
+class KthMaxMin {
+
+
+}
+
+public class P3_kth_max_min_ {
+
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        
+        new InputArray(arr, sc);
+
+        sc.close();
+    }
+    
+}
